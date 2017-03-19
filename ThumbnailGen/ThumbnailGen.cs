b970ce0831766d472928ba16008a3110ee1c0483
Frederik Gelder <frederik.gelder@freenet.de>
2017-03-19 19:58:52 +0100

    craptastic... this will be way harder than expected cause you only get a stream, and not a file for generating the thumb. so option a) fail and retreat, or b) make a (partial) copy of the file, which will kill your ssd in no time -.-

--- ThumbnailGen/ThumbnailGen.cs
+++ ThumbnailGen/ThumbnailGen.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
+using SharpShell.Attributes;
+using SharpShell.SharpThumbnailHandler;
 
 namespace ThumbnailGen
 {
     [ComVisible(true)]
-    public class ThumbnailGen
+    [COMServerAssociation(AssociationType.FileExtension, ".mov")]
+    public class ThumbnailGen : SharpThumbnailHandler
     {
+        protected override Bitmap GetThumbnailImage(uint width)
+        {
+            return null;
+        }
     }
 }
